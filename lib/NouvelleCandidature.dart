6524ed76d6f84f7cedc9f57cfa36b0125d35041a
fatima6299 <123902203+fatima6299@users.noreply.github.com>
2023-02-18 21:38:32 +0000

    Add files via upload

--- lib/NouvelleCandidature.dart
+++ lib/NouvelleCandidature.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projet_flutter/liste.dart';
+import 'package:projet_flutter/Testfatima.dart';
 
 void main() {
   runApp(const MyCandidature(
@@ -26,7 +26,8 @@ class _MyCandidatureState extends State<MyCandidature> {
     'Refusé',
     'Annulé'
   ];
-
+  final _formKey = GlobalKey<FormState>();
+  late String _email;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,76 +41,77 @@ class _MyCandidatureState extends State<MyCandidature> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
-             const TextField(
+            const TextField(
               decoration: InputDecoration(
                   prefixIcon: Icon(Icons.work),
-                  hintText: 'job',
-                  border: UnderlineInputBorder()),
+                  labelText: 'job',
+                  hintText: 'indiquer votre job',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(height: 10),
-             const TextField(
+            const TextField(
               decoration: InputDecoration(
                   prefixIcon: Icon(Icons.person),
-                 hintText: 'Nom',
-                  border: UnderlineInputBorder()),
-            ),
-            
-            const SizedBox(height: 10),
-              const TextField(
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  hintText: 'Nom complet',
-                  border: UnderlineInputBorder()),
+                  labelText: 'Nom complet',
+                  hintText: 'entrer votre nom ',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(height: 10),
-             const TextField(
+            const TextField(
               decoration: InputDecoration(
                   prefixIcon: Icon(Icons.email),
-                  hintText: 'Email',
-                  border: UnderlineInputBorder()),
+                  labelText: 'EMail',
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  hintText: 'indiquer votre Email',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(height: 10),
-             const TextField(
+            const TextField(
               decoration: InputDecoration(
                   prefixIcon: Icon(Icons.phone),
-                  hintText: 'Telephone',
-                  border: UnderlineInputBorder()),
+                  labelText: 'Telephone',
+                  hintText: 'indiquer votre numero Telephone',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(height: 10),
             const TextField(
               decoration: InputDecoration(
                   prefixIcon: Icon(Icons.calendar_today),
-                  hintText: 'Date de naissance',
-                  border: UnderlineInputBorder()),
+                  labelText: 'Date de naissance',
+                  hintText: 'indiquer votre date de naissance',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(height: 10),
             const TextField(
               decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.monetization_on),
-                  hintText: 'Renumeration demandée',
-                  border: UnderlineInputBorder()),
+                  // prefixIcon: Icon(Icons.monetization_on),
+                  labelText: 'Renumeration demandée',
+                  // hintText: 'indiquer votre date de naissance',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(height: 10),
             const TextField(
               decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.attach_money_rounded),
-                  hintText: 'Renumeration proposée',
-                  border: UnderlineInputBorder()),
+                  // prefixIcon: Icon(Icons.attach_money_rounded),
+                  labelText: 'Renumeration proposée',
+                  // hintText: 'indiquer votre date de naissance',
+                  border: OutlineInputBorder()),
             ),
-             
             const SizedBox(height: 10),
             const TextField(
                 maxLines: 5,
                 decoration: InputDecoration(
-                    hintText: 'Description',
+                    labelText: 'Description',
+                    //hintText: 'ajouter une description',
                     border: OutlineInputBorder())),
             const SizedBox(height: 10),
             const TextField(
                 decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.calendar_today),
-                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                    hintText: 'Date de création',
-                    border: UnderlineInputBorder())),
+                    suffixIcon: Icon(Icons.calendar_today),
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    labelText: 'Date de création',
+                    hintText: 'indiquer la date de creation',
+                    border: OutlineInputBorder())),
             const SizedBox(height: 10),
             const Text("Etat :",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
@@ -133,12 +135,7 @@ class _MyCandidatureState extends State<MyCandidature> {
                       padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
                       backgroundColor: MaterialStatePropertyAll(
                           Color.fromARGB(255, 98, 168, 226))),
-                  onPressed: () {
-                  Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyLinks()),
-            );
-                  },
+                  onPressed: () {},
                   child: const Text('Annuler'),
                 ),
                 ElevatedButton(
@@ -147,12 +144,41 @@ class _MyCandidatureState extends State<MyCandidature> {
                       backgroundColor: MaterialStatePropertyAll(
                           Color.fromARGB(255, 98, 168, 226))),
                   onPressed: () {
-                    
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Mytestf()));
                   },
                   child: const Text('Creer'),
                 ),
               ],
             )
+
+            /* Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 98, 168, 226))),
+                onPressed: () {},
+                child: 
+                const Text("Creer"),
+                
+              ),
+            ),
+            Container(
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 98, 168, 226))),
+                onPressed: () {},
+                child: const Text("Annuler"),
+              ),
+            ),
+            */
           ],
         )),
       ),
